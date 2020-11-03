c0049e283857c93785ec4c9515ec796af168434f
jialin guo <guoj@cern.ch>
2020-11-03 03:52:24 +0100

    new file

--- condorFile/runCondor.sh
+++ condorFile/runCondor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting job"
 echo "copying proxy file to /tmp area"
-cp x509up_u117617 /tmp/x509up_u117617
+cp x509up_u123627 /tmp/x509up_u123627
 echo "copy done..."
 echo "start running"
 python codeFile/Getvariables.py $*

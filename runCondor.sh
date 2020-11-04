2d15782ad50b9233742bfc93d02d37f49b708109
jialin guo <guoj@cern.ch>
2020-11-04 02:36:46 +0100

    22

--- runCondor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting job"
-echo "copying proxy file to /tmp area"
-cp x509up_u117617 /tmp/x509up_u117617
-echo "copy done..."
-echo "start running"
-python codeFile/Getvariables.py $*
-echo "running done"

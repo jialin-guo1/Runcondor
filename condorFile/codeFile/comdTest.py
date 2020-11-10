67d82fa3a76c91311a50beaf40e596ede61471c6
郭佳林 <rogerguo@guojialindeMacBook-Pro.local>
2020-11-10 21:59:46 +0800

    fix 5

--- condorFile/codeFile/comdTest.py
+++ condorFile/codeFile/comdTest.py
@@ -5,5 +5,6 @@ parser = argparse.ArgumentParser(description="A simple ttree plotter")
 parser.add_argument("-i", "--inputfiles", dest="inputfiles", default="Sync_1031_2018_ttH_v2.root", help="List of input files")
 args = parser.parse_args()
 
-for filename in args.inputfiles:
+file = commands.getstatusoutput('ls'+str(args.inputfiles))
+for filename file:
     print "filename="+str(filename)

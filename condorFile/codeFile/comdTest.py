c703bcd94fe62dd63704fb1166962664a3218156
郭佳林 <rogerguo@guojialindeMacBook-Pro.local>
2020-11-10 23:41:31 +0800

    fix 17

--- condorFile/codeFile/comdTest.py
+++ condorFile/codeFile/comdTest.py
@@ -9,7 +9,7 @@ args = parser.parse_args()
 def ifROOT(line):
     line=line.strip('\n')
     print line[-3:]
-    if line[-3:] == [l,o,g]:
+    if line[-3:] == "log":
         return False
 
 outputfile = os.popen('xrdfs root://cmsio5.rc.ufl.edu/ ls  '+str(args.inputfiles))

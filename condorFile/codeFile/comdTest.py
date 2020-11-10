465c0201d322c9cca38f71c87d0136e92de8d0fb
郭佳林 <rogerguo@guojialindeMacBook-Pro.local>
2020-11-10 23:25:11 +0800

    fix 13

--- condorFile/codeFile/comdTest.py
+++ condorFile/codeFile/comdTest.py
@@ -1,10 +1,11 @@
 import commands
+import os
 
 import argparse
 parser = argparse.ArgumentParser(description="A simple ttree plotter")
 parser.add_argument("-i", "--inputfiles", dest="inputfiles", default="Sync_1031_2018_ttH_v2.root", help="List of input files")
 args = parser.parse_args()
 
-status, outputfile = commands.getstatusoutput('xrdfs root://cmsio5.rc.ufl.edu/ ls  '+str(args.inputfiles))
-for filename in outputfile:
-    print "filename="+str(filename)
+outputfile = os.popen('xrdfs root://cmsio5.rc.ufl.edu/ ls  '+str(args.inputfiles))
+for line in outputfile:
+    print "filename="+str(line.strip('\n'))

7c031216e4553887bb67f96b944a8a3551ce357e
郭佳林 <rogerguo@guojialindeMacBook-Pro.local>
2020-11-11 11:13:50 +0800

    fix 35

--- condorFile/codeFile/comdTest.py
+++ condorFile/codeFile/comdTest.py
@@ -4,19 +4,24 @@ import os
 import argparse
 parser = argparse.ArgumentParser(description="A simple ttree plotter")
 parser.add_argument("-i", "--inputfiles", dest="inputfiles", default="Sync_1031_2018_ttH_v2.root", help="List of input files")
+parser.add_argument("-t", "--ttree", dest="ttree", default="Ana/passedEvents", help="TTree Name")
 args = parser.parse_args()
+
+import ROOT
+chain = ROOT.TChain(args.ttree)
+chain.Add("root://cms-xrd-global.cern.ch/"+args.inputfiles+"/*.root")
+print 'Total number of events: ' + str(chain.GetEntries())
 
 def ifROOT(line):
     line=line.strip('\n')
-#    print line[-3:]
     if line[-4:] != "root":
         return False
-
-print "root://cms-xrd-global.cern.ch/"+args.inputfiles+"/*.root"
 
 outputfile = os.popen('xrdfs root://cmsio5.rc.ufl.edu/ ls  '+str(args.inputfiles))
 for line in outputfile:
     if(ifROOT(line)==False):
         continue
+    line=line.strip('\n')
+    files = ROOT.TFile("root://cms-xrd-global.cern.ch/"+line)
     print "root://cms-xrd-global.cern.ch/"+line
 #    print "filename="+str(line.strip('\n'))

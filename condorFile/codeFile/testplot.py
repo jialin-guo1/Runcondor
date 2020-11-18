0f8862d1fee7aad1563c0770d35a742e5f24094f
jialin-guo1 <jialin.guo@cern.ch>
2020-11-18 22:00:58 +0800

    zx 17

--- condorFile/codeFile/testplot.py
+++ condorFile/codeFile/testplot.py
@@ -6,5 +6,6 @@ h = ROOT.TH1D("h","h",50,70,170)
 for ievent,event in enumerate(t):
     if(not event.passedFullSelection): continue
     h.Fill(event.H_FSR)
-
+c = ROOT.TCanvas()
 h.Draw("E1")
+c.SaveAs("testplot.png")

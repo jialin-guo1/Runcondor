26c421eb92b47c17caae3fb1baa195820c50f137
jialin-guo1 <jialin.guo@cern.ch>
2020-11-18 21:45:21 +0800

    zx 14

--- condorFile/codeFile/testBool.py
+++ condorFile/codeFile/testBool.py
@@ -17,30 +17,38 @@ print("get file " + str(args.inputfiles))
 chain = ROOT.TChain(args.ttree)
 chain.Add(args.inputfiles)
 
+passedFullSelection = array('i',[-90])
+passedTrig = array('i',[-90])
 passedZXCRSelection = array('i',[-90])
 nZXCRFailedLeptons = array('i',[-90])
+H_FSR = array('f',[0])
 
 file_out = ROOT.TFile(args.outputfile, 'recreate')
 passedEvents = ROOT.TTree("passedEvents","passedEvents")
 passedEvents.Branch("passedZXCRSelection",passedZXCRSelection,"passedZXCRSelection/I")
 passedEvents.Branch("nZXCRFailedLeptons",nZXCRFailedLeptons,"nZXCRFailedLeptons/I")
+passedEvents.Branch("passedFullSelection",passedFullSelection,"passedFullSelection/I")
+passedEvents.Branch("passedTrig",passedTrig,"passedTrig/I")
+passedEvents.Branch("H_FSR",H_FSR,"H_FSR/F")
 
 for ievent,event in enumerate(chain):
     if ievent == 50000: break
-    if(event.passedTrig):
-        if(event.passedFullSelection):
-            for i in range(event.lep_RelIsoNoFSR.size()):
-                print "passedTrig = " + str(event.passedTrig)
-                print "passedFullSelection = " + str(event.passedFullSelection)
+    passedZXCRSelection[0] = event.passedZXCRSelection
+    nZXCRFailedLeptons[0] = event.nZXCRFailedLeptons
+    passedFullSelection[0] = event.passedFullSelection
+    passedTrig[0] = event.passedTrig
+    l1FSR = ROOT.TLorentzVector()
+    l2FSR = ROOT.TLorentzVector()
+    l3FSR = ROOT.TLorentzVector()
+    l4FSR = ROOT.TLorentzVector()
+    l1FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[0]],event.lepFSR_eta[event.lep_Hindex[0]],event.lepFSR_phi[event.lep_Hindex[0]],event.lepFSR_mass[event.lep_Hindex[0]])
+    l2FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[1]],event.lepFSR_eta[event.lep_Hindex[1]],event.lepFSR_phi[event.lep_Hindex[1]],event.lepFSR_mass[event.lep_Hindex[1]])
+    l3FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[2]],event.lepFSR_eta[event.lep_Hindex[2]],event.lepFSR_phi[event.lep_Hindex[2]],event.lepFSR_mass[event.lep_Hindex[2]])
+    l4FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[3]],event.lepFSR_eta[event.lep_Hindex[3]],event.lepFSR_phi[event.lep_Hindex[3]],event.lepFSR_mass[event.lep_Hindex[3]])
+    H4massFSR = ROOT.TLorentzVector()
+    H4massFSR = l1FSR+l2FSR+l3FSR+l4FSR
+    H_FSR[0] = H4massFSR.M()
     passedEvents.Fill()
-
-for ievent,event in enumerate(chain):
-    if ievent == 50000: break
-    if(event.passedZXCRSelection):
-        passedZXCRSelection[0] = event.passedZXCRSelection
-        nZXCRFailedLeptons[0] = event.nZXCRFailedLeptons
-    passedEvents.Fill()
-
 
 file_out.Write()
 file_out.Close()

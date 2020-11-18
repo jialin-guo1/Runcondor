6a176a130be7f6bd1fabbcb7803ac13c737152ce
jialin-guo1 <jialin.guo@cern.ch>
2020-11-18 17:20:43 +0800

    all

--- condorFile/codeFile/GetvariablesIR0.py
+++ condorFile/codeFile/GetvariablesIR0.py
@@ -109,14 +109,14 @@ k_qq_qcd_M = array('f',[0.])
 k_qq_ewk = array('f',[0.])
 k_qq_qcd_pt = array('f',[0.])
 cross = array('f',[0.])
-Cat = array('l',[0])
+Cat = array('i',[0])
 #ZX
 lep_RelIsoNoFSR1 = array('f',[0.])
 lep_RelIsoNoFSR2 = array('f',[0.])
 lep_RelIsoNoFSR3 = array('f',[0.])
 lep_RelIsoNoFSR4 = array('f',[0.])
-passedZXCRSelection = array('l',[0])
-nZXCRFailedLeptons = array('l',[0])
+passedZXCRSelection = array('i',[0])
+nZXCRFailedLeptons = array('i',[0])
 
 
 
@@ -178,8 +178,8 @@ passedEvents.Branch("lep_RelIsoNoFSR1",lep_RelIsoNoFSR1,"lep_RelIsoNoFSR1/F")
 passedEvents.Branch("lep_RelIsoNoFSR2",lep_RelIsoNoFSR2,"lep_RelIsoNoFSR1/F")
 passedEvents.Branch("lep_RelIsoNoFSR3",lep_RelIsoNoFSR3,"lep_RelIsoNoFSR1/F")
 passedEvents.Branch("lep_RelIsoNoFSR4",lep_RelIsoNoFSR4,"lep_RelIsoNoFSR1/F")
-passedEvents.Branch("passedZXCRSelection",passedZXCRSelection,"passedZXCRSelection/O")
-passedEvents.Branch("nZXCRFailedLeptons",nZXCRFailedLeptons,"nZXCRFailedLeptons/s")
+passedEvents.Branch("passedZXCRSelection",passedZXCRSelection,"passedZXCRSelection/I")
+passedEvents.Branch("nZXCRFailedLeptons",nZXCRFailedLeptons,"nZXCRFailedLeptons/I")
 
 #Loop over all the events in the input ntuple
 for ievent,event in enumerate(chain):

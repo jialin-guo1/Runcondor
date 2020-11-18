3e166ba7a9e102130a9ed64f527c35cf0176fa37
jialin-guo1 <jialin.guo@cern.ch>
2020-11-18 22:37:18 +0800

    zx 19

--- condorFile/codeFile/Getvariables.py
+++ condorFile/codeFile/Getvariables.py
@@ -82,6 +82,8 @@ lep_RelIsoNoFSR3 = array('f',[0.])
 lep_RelIsoNoFSR4 = array('f',[0.])
 passedZXCRSelection = array('i',[0])
 nZXCRFailedLeptons = array('i',[0])
+passedFullSelection = array('i',[0])
+passedTrig = array('i',[0])
 
 
 
@@ -145,14 +147,15 @@ passedEvents.Branch("lep_RelIsoNoFSR3",lep_RelIsoNoFSR3,"lep_RelIsoNoFSR1/F")
 passedEvents.Branch("lep_RelIsoNoFSR4",lep_RelIsoNoFSR4,"lep_RelIsoNoFSR1/F")
 passedEvents.Branch("passedZXCRSelection",passedZXCRSelection,"passedZXCRSelection/I")
 passedEvents.Branch("nZXCRFailedLeptons",nZXCRFailedLeptons,"nZXCRFailedLeptons/I")
+passedEvents.Branch("passedFullSelection",passedFullSelection,"passedFullSelection/I")
+passedEvents.Branch("passedTrig",passedTrig,"passedTrig/I")
 
 #Loop over all the events in the input ntuple
 for ievent,event in enumerate(chain):
     passedZXCRSelection[0] = event.passedZXCRSelection
     nZXCRFailedLeptons[0] = event.nZXCRFailedLeptons
-    if(not event.passedTrig): continue
-    if(not event.passedFullSelection): continue
-
+    passedFullSelection[0] = event.passedFullSelection
+    passedTrig[0] = event.passedTrig
     for i in range(event.lep_RelIsoNoFSR.size()):
         lep_RelIsoNoFSR1[0] = event.lep_RelIsoNoFSR[event.lep_Hindex[0]]
         lep_RelIsoNoFSR2[0] = event.lep_RelIsoNoFSR[event.lep_Hindex[1]]
@@ -178,72 +181,71 @@ for ievent,event in enumerate(chain):
     Nlep = event.lep_pt.size()
     for i in range(Nlep):
 
-    #fill tree
-
-       lep1_pt[0] = event.lep_pt[event.lep_Hindex[0]]
-       lep1_eta[0] = event.lep_eta[event.lep_Hindex[0]]
-       lep1_phi[0] = event.lep_phi[event.lep_Hindex[0]]
-       lep1_mass[0] = event.lep_mass[event.lep_Hindex[0]]
+        lep1_pt[0] = event.lep_pt[event.lep_Hindex[0]]
+        lep1_eta[0] = event.lep_eta[event.lep_Hindex[0]]
+        lep1_phi[0] = event.lep_phi[event.lep_Hindex[0]]
+        lep1_mass[0] = event.lep_mass[event.lep_Hindex[0]]
 
 
-       lep2_pt[0] = event.lep_pt[event.lep_Hindex[1]]
-       lep2_eta[0] = event.lep_eta[event.lep_Hindex[1]]
-       lep2_phi[0] = event.lep_phi[event.lep_Hindex[1]]
-       lep2_mass[0] = event.lep_mass[event.lep_Hindex[1]]
+        lep2_pt[0] = event.lep_pt[event.lep_Hindex[1]]
+        lep2_eta[0] = event.lep_eta[event.lep_Hindex[1]]
+        lep2_phi[0] = event.lep_phi[event.lep_Hindex[1]]
+        lep2_mass[0] = event.lep_mass[event.lep_Hindex[1]]
 
-       lep3_pt[0] = event.lep_pt[event.lep_Hindex[2]]
-       lep3_eta[0] = event.lep_eta[event.lep_Hindex[2]]
-       lep3_phi[0] = event.lep_phi[event.lep_Hindex[2]]
-       lep3_mass[0] = event.lep_mass[event.lep_Hindex[2]]
+        lep3_pt[0] = event.lep_pt[event.lep_Hindex[2]]
+        lep3_eta[0] = event.lep_eta[event.lep_Hindex[2]]
+        lep3_phi[0] = event.lep_phi[event.lep_Hindex[2]]
+        lep3_mass[0] = event.lep_mass[event.lep_Hindex[2]]
 
-       lep4_pt[0] = event.lep_pt[event.lep_Hindex[3]]
-       lep4_eta[0] = event.lep_eta[event.lep_Hindex[3]]
-       lep4_phi[0] = event.lep_phi[event.lep_Hindex[3]]
-       lep4_mass[0] = event.lep_mass[event.lep_Hindex[3]]
+        lep4_pt[0] = event.lep_pt[event.lep_Hindex[3]]
+        lep4_eta[0] = event.lep_eta[event.lep_Hindex[3]]
+        lep4_phi[0] = event.lep_phi[event.lep_Hindex[3]]
+        lep4_mass[0] = event.lep_mass[event.lep_Hindex[3]]
 
-       lep1FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[0]]
-       lep1FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[0]]
-       lep1FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[0]]
-       lep1FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[0]]
+        lep1FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[0]]
+        lep1FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[0]]
+        lep1FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[0]]
+        lep1FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[0]]
 
-       lep2FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[1]]
-       lep2FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[1]]
-       lep2FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[1]]
-       lep2FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[1]]
+        lep2FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[1]]
+        lep2FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[1]]
+        lep2FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[1]]
+        lep2FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[1]]
 
-       lep3FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[2]]
-       lep3FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[2]]
-       lep3FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[2]]
-       lep3FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[2]]
+        lep3FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[2]]
+        lep3FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[2]]
+        lep3FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[2]]
+        lep3FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[2]]
 
-       lep4FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[3]]
-       lep4FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[3]]
-       lep4FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[3]]
-       lep4FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[3]]
+        lep4FSR_pt[0] = event.lepFSR_pt[event.lep_Hindex[3]]
+        lep4FSR_eta[0] = event.lepFSR_eta[event.lep_Hindex[3]]
+        lep4FSR_phi[0] = event.lepFSR_phi[event.lep_Hindex[3]]
+        lep4FSR_mass[0] = event.lepFSR_mass[event.lep_Hindex[3]]
 
-       l1 = ROOT.TLorentzVector()
-       l2 = ROOT.TLorentzVector()
-       l3 = ROOT.TLorentzVector()
-       l4 = ROOT.TLorentzVector()
-       l1.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[0]],event.lep_eta[event.lep_Hindex[0]],event.lep_phi[event.lep_Hindex[0]],event.lep_mass[event.lep_Hindex[0]])
-       l2.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[1]],event.lep_eta[event.lep_Hindex[1]],event.lep_phi[event.lep_Hindex[1]],event.lep_mass[event.lep_Hindex[1]])
-       l3.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[2]],event.lep_eta[event.lep_Hindex[2]],event.lep_phi[event.lep_Hindex[2]],event.lep_mass[event.lep_Hindex[2]])
-       l4.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[3]],event.lep_eta[event.lep_Hindex[3]],event.lep_phi[event.lep_Hindex[3]],event.lep_mass[event.lep_Hindex[3]])
-       H4mass = ROOT.TLorentzVector()
-       H4mass = l1+l2+l3+l4
-       H[0] = H4mass.M()
+        l1 = ROOT.TLorentzVector()
+        l2 = ROOT.TLorentzVector()
+        l3 = ROOT.TLorentzVector()
+        l4 = ROOT.TLorentzVector()
+        l1.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[0]],event.lep_eta[event.lep_Hindex[0]],event.lep_phi[event.lep_Hindex[0]],event.lep_mass[event.lep_Hindex[0]])
+        l2.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[1]],event.lep_eta[event.lep_Hindex[1]],event.lep_phi[event.lep_Hindex[1]],event.lep_mass[event.lep_Hindex[1]])
+        l3.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[2]],event.lep_eta[event.lep_Hindex[2]],event.lep_phi[event.lep_Hindex[2]],event.lep_mass[event.lep_Hindex[2]])
+        l4.SetPtEtaPhiM(event.lep_pt[event.lep_Hindex[3]],event.lep_eta[event.lep_Hindex[3]],event.lep_phi[event.lep_Hindex[3]],event.lep_mass[event.lep_Hindex[3]])
+        H4mass = ROOT.TLorentzVector()
+        H4mass = l1+l2+l3+l4
+        H[0] = H4mass.M()
 
-       l1FSR = ROOT.TLorentzVector()
-       l2FSR = ROOT.TLorentzVector()
-       l3FSR = ROOT.TLorentzVector()
-       l4FSR = ROOT.TLorentzVector()
-       l1FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[0]],event.lepFSR_eta[event.lep_Hindex[0]],event.lepFSR_phi[event.lep_Hindex[0]],event.lepFSR_mass[event.lep_Hindex[0]])
-       l2FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[1]],event.lepFSR_eta[event.lep_Hindex[1]],event.lepFSR_phi[event.lep_Hindex[1]],event.lepFSR_mass[event.lep_Hindex[1]])
-       l3FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[2]],event.lepFSR_eta[event.lep_Hindex[2]],event.lepFSR_phi[event.lep_Hindex[2]],event.lepFSR_mass[event.lep_Hindex[2]])
-       l4FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[3]],event.lepFSR_eta[event.lep_Hindex[3]],event.lepFSR_phi[event.lep_Hindex[3]],event.lepFSR_mass[event.lep_Hindex[3]])
-       H4massFSR = ROOT.TLorentzVector()
-       H4massFSR = l1FSR+l2FSR+l3FSR+l4FSR
-       H_FSR[0] = H4massFSR.M()
+        l1FSR = ROOT.TLorentzVector()
+        l2FSR = ROOT.TLorentzVector()
+        l3FSR = ROOT.TLorentzVector()
+        l4FSR = ROOT.TLorentzVector()
+        l1FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[0]],event.lepFSR_eta[event.lep_Hindex[0]],event.lepFSR_phi[event.lep_Hindex[0]],event.lepFSR_mass[event.lep_Hindex[0]])
+        l2FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[1]],event.lepFSR_eta[event.lep_Hindex[1]],event.lepFSR_phi[event.lep_Hindex[1]],event.lepFSR_mass[event.lep_Hindex[1]])
+        l3FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[2]],event.lepFSR_eta[event.lep_Hindex[2]],event.lepFSR_phi[event.lep_Hindex[2]],event.lepFSR_mass[event.lep_Hindex[2]])
+        l4FSR.SetPtEtaPhiM(event.lepFSR_pt[event.lep_Hindex[3]],event.lepFSR_eta[event.lep_Hindex[3]],event.lepFSR_phi[event.lep_Hindex[3]],event.lepFSR_mass[event.lep_Hindex[3]])
+        H4massFSR = ROOT.TLorentzVector()
+        H4massFSR = l1FSR+l2FSR+l3FSR+l4FSR
+        H_FSR[0] = H4massFSR.M()
+
 
 
 
@@ -255,6 +257,7 @@ for ievent,event in enumerate(chain):
         h_mass[0] = event.H_mass[i]
 #      lep_Hindex[i] = event.lep_Hindex[i]
     passedEvents.Fill()
+
 
 file_out.Write()
 file_out.Close()
